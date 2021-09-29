c6d5f0c53a51080c11abbe14219cc88510c481f6
Mario Morales <mario.morales@devsu.com>
2021-09-29 10:26:16 -0500

    adding components, directives and bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,19 @@ import { Component } from '@angular/core';
 @Component({
    selector: 'app-root',
    templateUrl: './app.component.html',
-   styleUrls: ['./app.component.css']
+   styleUrls: ['./app.component.css'],
+   // styles: [`
+   //    h3 {
+   //       color: dodgerblue;
+   //    }
+   // `]
 })
 export class AppComponent {
+   showSecret = false;
+   log = [];
+
+   toggleDetails() {
+      this.showSecret = !this.showSecret;
+      this.log.push(this.log.length + 1);
+   }
 }
